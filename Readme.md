5a472cc29441470578dafc3682ede56a4d343cc8
Abir <abir.pal899@gmail.com>
2020-10-13 12:24:44 +0530

    Readme Updated with API and Installation 😎😎

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# CertiGen
+```js
+WIP: "Azure Blobs Connection"
+
+```
+<p align="center">
+<img src="./images/Banner.png">
+</p>
+
+
+# Installation & API
+
+### Installation
+<hr/>
+
+1. Clone this repository.
+2. Install npm packages using `npm i`
+3. Run the server. `npm run devStart`
+4. Server starts running at port: 5000
+5. Go to Browser and run `localhost:5000`
+6. You will see the usage of API and Help on your browser.
+
+### API
+<hr/>
+<ul><li><b>GET '/'</b> : This is a Help Route</li> <li><b>POST '/sendData'</b> : One and only Route for Certificate Generation. Below is the description of what you should send as Raw JSON with this POST Request. <br/><ul><li>{<br/> &nbsp; <b>event_name</b>:\"Name of The Event",<br/>&nbsp; <b> ambassador_name</b>: "Name of the Ambassador\",<br/>&nbsp; <b>participants</b>:[\"Person 1",\"Person 2\",\"Person 3","Person 4"]<br/>}</li><ul></li></ul><li> After Successful generation of certificates you will receive response as <b>Success</b> and all certificates will be in folder named <b>certificates</b></li>
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express')
 // const bodyParser = require('body-parser')
 const cors = require('cors')
 const port=5000
+const jsonExamplemessage=`{<br/> &nbsp; <b>event_name</b>:\"Name of The Event",<br/>&nbsp; <b> ambassador_name</b>: "Name of the Ambassador\",<br/>&nbsp; <b>participants</b>:[\"Person 1",\"Person 2\",\"Person 3","Person 4"]<br/>}`
+const getMessage=`<center><h2> This is backend API for Certigen:</h2><h3> A Certificate Generator exclusively for MSFT Ambassador Events </h3><br><h1> 😁You are at route '/'<h1> <br><br> <h2>🧏‍♂️Here are the list of things you can do with this API for now.</center> </h2> <ul><li><b>GET '/'</b> : You are now on Help Route</li> <li><b>POST '/sendData'</b> : One and only Route for Certificate Generation. Below is the description of what you should send as Raw JSON with this POST Request. <br/><ul><li>${jsonExamplemessage}</li><ul></li></ul><li> After Successful generation of certificates you will receive response as <b>Success</b> and all certificates will be in folder named <b>certificates</b></li>`;
+
+
+
 const app=express()
 app.use(express.raw())
 app.use(express.json())
@@ -11,7 +16,7 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.get('/',(req,res)=>{
-    res.send("API For generator")
+    res.send(getMessage)
 })
 app.post('/sendData',(req,res)=>{
                     try {
